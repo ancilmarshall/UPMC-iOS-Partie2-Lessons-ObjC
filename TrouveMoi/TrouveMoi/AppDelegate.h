9b911c7da7fd98afc972b7a1871a95766cab0c6c
Ancil <ancilm2000@yahoo.com>
2015-04-27 12:36:13 +0200

    Create empty TrouveMoi project

---
+++ TrouveMoi/TrouveMoi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrouveMoi
+//
+//  Created by Ancil on 4/27/15.
+//  Copyright (c) 2015 Ancil Marshall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TrouveMoi/TrouveMoi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TrouveMoi
+//
+//  Created by Ancil on 4/27/15.
+//  Copyright (c) 2015 Ancil Marshall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
